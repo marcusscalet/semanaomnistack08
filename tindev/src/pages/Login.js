d75aed61c052159370644e72c8f7b28623c68f84
marcusscalet <marcus.scalet@hotmail.com>
2019-08-09 23:41:11 -0300

    criada branch develop e iniciado projeto em react-native

---
+++ tindev/src/pages/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Login(){
+    return (
+    <View style={styles.container}>
+
+    </View>
+    );
+}
+
+const styles = Stylesheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#7159c1',
+    justifyContente: 'center',
+    alignItems: 'center'
+  },
+
+  text:{
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize:20
+  },
+});
+
+export default App;
